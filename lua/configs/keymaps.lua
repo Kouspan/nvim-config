04026d6a8ae929380879b94163d141b6126f06fd
Panagiotis Kousidis <panos4266@gmail.com>
2023-12-16 22:08:09 +0200

    more keybinds

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -3,3 +3,8 @@ vim.keymap.set('n', '<Space>', '<Nop>', {silent = true})
 vim.g.mapleader = ' '
 vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('i', 'kj', '<Esc>')
+
+--COPY,MOVE--
+vim.keymap.set('n', '<C-D>', '<CMD>t .<CR>')
+vim.keymap.set('n', '<C-K>', '<CMD>mo -2<CR>')
+vim.keymap.set('n', '<C-J>', '<CMD>mo +1<CR>')
