34dd8cea86befee27c3980e3b8331dc43e23210c
Panagiotis Kousidis <panos4266@gmail.com>
2023-12-21 16:43:55 +0200

    fix typo

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,6 @@
 return {
 	'nvim-lualine/lualine.nvim',
-	depedencies = { 'nvim-tree/nvim-web-devicons' },
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
 	opts = { 
 		options = {
 			icons_enabled = true,
