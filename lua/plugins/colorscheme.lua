1df1b8cba0d03042892711812c9736fc928a6074
Panagiotis Kousidis <panos4266@gmail.com>
2023-12-23 23:03:09 +0200

    cosmetic changes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,21 +1,34 @@
-return{
-	{
-		"catppuccin/nvim",
-		lazy = true,
-		name = "catppuccin",
-		priority = 1000,
-	},
-	{
-		"navarasu/onedark.nvim",
-		lazy=false,
-		name="onedark",
-		priority=1000,
-		config = function()
-			M = require("onedark")
-			M.setup({style = "darker"})
-			M.load()
-		end
-	}
+return {
+  {
+    "navarasu/onedark.nvim",
+    lazy = true,
+    name = "onedark",
+    priority = 1000,
+    config = function()
+      M = require("onedark")
+      M.setup({ style = "darker" })
+      M.load()
+    end
+  },
+  {
+    'marko-cerovac/material.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.material_style = "palenight"
+      local opts         = {}
+      opts.high_visibility = { darker = true}
+      opts.contrast      = { cursor_line = false }
+      opts.plugins       = {
+        "nvim-cmp",
+        "nvim-web-devicons",
+        "telescope",
+        "which-key"
+      }
+      opts.lualine_style = "stealth"
+      opts.async_loading = true
+      require("material").setup(opts)
+      vim.cmd 'colorscheme material'
+    end
+  }
 }
-
-
