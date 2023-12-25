7e960d8884b07d6463a1c5883185e65bd399aa81
Panagiotis Kousidis <panos4266@gmail.com>
2023-12-25 15:43:17 +0200

    selection move keymaps

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -1,13 +1,20 @@
-vim.keymap.set('n', '<Space>', '<Nop>', {silent = true})
+vim.keymap.set('i', '<C-J>', '<Nop>', {silent = true})
 
 vim.g.mapleader = ' '
 vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('i', 'kj', '<Esc>')
+vim.keymap.set('n', '<Space>', '<Nop>', {silent = true})
 
 --COPY,MOVE--
+-- duplication
 vim.keymap.set('n', '<C-D>', '<CMD>t .<CR>')
+-- move line
 vim.keymap.set('n', '<C-K>', '<CMD>mo -2<CR>')
 vim.keymap.set('n', '<C-J>', '<CMD>mo +1<CR>')
+-- move selection
+vim.keymap.set('v', '<C-J>', ':mo \'>+1<CR>gv')
+vim.keymap.set('v', '<C-K>', ':mo \'<-2<CR>gv')
+-- insert new line in normal mode
 vim.keymap.set('n', '<CR>', 'i<CR><Esc>')
 
 --BUFFERS--
